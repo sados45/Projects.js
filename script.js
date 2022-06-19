ddbf0ae1c1bf3ce9438346266c5cf93242c525ac
sados45 <sados45@gmail.com>
2022-06-19 02:48:55 +0200

    hava durumu

---
+++ script.js
@@ -0,0 +1,43 @@
+const url = 'https://api.openweathermap.org/data/3.0/'
+
+const key= 'bcefe7862c8f439632389ac930d21d3b'
+
+const setQuery = (e) => {
+
+        if(e.keyCode == '13')
+            getResult(searchBar.value)
+
+}
+
+const getResult = (cityName) => {
+
+    let query = '${url} weather? q = $ {cityName} & appid= ${key} & units = metric&lang=tr'
+
+    fetch(query)
+   .then(weather =>{
+        return weather.json()
+   })
+   .then(displayResult)
+}
+
+    const displayResult =(result) =>{
+       
+    let city = document.querySelector('.city')
+
+    city.innerText ='${result.name}, ${result.sys.country}'
+
+    let temp = document.querySelector('.temp')
+    temp.innerText = '${Math.round(result.main.temp)}°c'
+
+    let desc = document.querySelector('.desc')
+    desc.innerText =result.weather[0].description
+    
+    let minmax = document.querySelector('.minmax')
+
+    minmax.innerText='${Math.round(result.main.temp_min)}°c /${Math.round(result.main.temp_max)}°c '
+
+}
+
+const searchBar = document.getElementById('searchBar')
+
+searchBar.addEventListener('keypress', setQuery)
